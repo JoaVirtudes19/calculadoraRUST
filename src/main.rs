f3eb90832b060dfa867f4ea13e4520fbc84fd828
JoaVirtudes19 <josejoaquinvirtudes@gmail.com>
2022-08-21 17:28:58 +0200

    Fixed arithmetic order

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use regex::Regex;
 
-fn main() {
-    
-    // Regex
-    let re_op:Regex= Regex::new(r"(\d+)\s*([\*\+\-])\s*(\d+)").unwrap();
+fn realiza_parentesis(mut operacion:String) -> String {
+    loop {
+        let re_op:Regex= Regex::new(r"\((.+)\)").unwrap();
+        let caps = re_op.captures(operacion.as_str());
+        if caps.is_none(){
+            break;
+        }
+        let caps = caps.unwrap(); //Hacemos unwrap
+        let mut sub_operacion = caps.get(1).unwrap().as_str().to_string();
+        // Parentesis
+        sub_operacion = realiza_parentesis(sub_operacion);
+        // Multiplicaciones
+        sub_operacion = realiza_operacion("*", sub_operacion);
+        // Divisiones
+        sub_operacion = realiza_operacion("/", sub_operacion);
+        // Sumas
+        sub_operacion = realiza_operacion("+", sub_operacion);
+        // Restas
+        sub_operacion = realiza_operacion("-", sub_operacion);
 
-    // Datos del usuario
-    println!("Introduce tu operación: ");
-    let mut operacion:String = String::new(); 
-    std::io::stdin().read_line(&mut operacion).unwrap();
+        //Una vez realizadas podemos operar normal
+        operacion = operacion.replace(caps.get(0).unwrap().as_str(), &sub_operacion.to_string());
+    }
+    return operacion;
+}
 
-    // Operaciones matemáticas
 
+fn realiza_operacion(op:&str,mut operacion:String) -> String{
+    let string_regex:String = if op == "/" {
+        r"(\d+)\s*/\s*(\d+)".to_string()
+    } else{
+        r"(\d+)\s*\".to_string()+op+&r"\s*(\d+)".to_string()
+    };
+
+    let re_op:Regex= Regex::new(&string_regex).unwrap();
     loop {
         let caps = re_op.captures(operacion.as_str());
         
@@ -22,8 +45,7 @@ fn main() {
         // Hacemos operaciones paso a paso
         let caps = caps.unwrap();
         let left : i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let op : &str = caps.get(2).unwrap().as_str();
-        let right : i32 = caps.get(3).unwrap().as_str().parse().unwrap();
+        let right : i32 = caps.get(2).unwrap().as_str().parse().unwrap();
 
         let aux_op : i32 = if op == "*" {
             left * right
@@ -37,7 +59,27 @@ fn main() {
 
         operacion = operacion.replace(caps.get(0).unwrap().as_str(), &aux_op.to_string())
     }
+    return operacion
+}
+fn main() {
+    
+    // Datos del usuario
+    println!("Introduce tu operación: ");
+    let mut operacion:String = String::new(); 
+    std::io::stdin().read_line(&mut operacion).unwrap();
 
+    // Operaciones matemáticas
+
+    // Parentesis
+    operacion = realiza_parentesis(operacion);
+    // Multiplicaciones
+    operacion = realiza_operacion("*", operacion);
+    // Divisiones
+    operacion = realiza_operacion("/", operacion);
+    // Sumas
+    operacion = realiza_operacion("+", operacion);
+    // Restas
+    operacion = realiza_operacion("-", operacion);
 
     //Mostramos resultados
     println!("Resultado: {}",operacion);
